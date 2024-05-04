c123eea93cb66f20e37c4d0d0aec0157b7089aa6
husseinmleng <hali12122015@gmail.com>
2024-05-05 00:39:06 +0300

    data types

---
+++ data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int num;
+    long long big_num;
+    char ch;
+    float decimal;
+    double big_decimal;
+    cin >> num >> big_num >> ch >> decimal >> big_decimal;
+
+    cout <<num << endl;
+    cout <<big_num << endl;
+    cout <<ch << endl;
+    cout <<decimal << endl;
+    cout <<big_decimal << endl;
+
+    return 0;
+}
+
